305cfbba0978134765bd43498e4e165739ea338d
Rudy Matela <rudy@matela.com.br>
2017-08-01 17:19:34 +0100

    eg/sorting: reindent to match README and Haddock

--- eg/sorting.hs
+++ eg/sorting.hs
@@ -3,25 +3,26 @@ import Test.LeanCheck.Utils
 import qualified Test.LeanCheck as Lean
 
 sort :: Ord a => [a] -> [a]
-sort [] = []
-sort (x:xs) = sort (filter (< x) xs)
-           ++ [x]
-           ++ sort (filter (> x) xs)
+sort []      =  []
+sort (x:xs)  =  sort (filter (< x) xs)
+             ++ [x]
+             ++ sort (filter (> x) xs)
 
 prop_sortOrdered :: Ord a => [a] -> Bool
-prop_sortOrdered xs = ordered (sort xs)
-  where
-  ordered (x:y:xs) = x <= y && ordered (y:xs)
-  ordered _ = True
+prop_sortOrdered xs  =  ordered (sort xs)
+
+
+ordered (x:y:xs) = x <= y && ordered (y:xs)
+ordered _ = True
 
 prop_sortCount :: Ord a => a -> [a] -> Bool
-prop_sortCount x xs = count x (sort xs) == count x xs
+prop_sortCount x xs  =  count x (sort xs) == count x xs
 
 count :: Eq a => a -> [a] -> Int
-count x = length . filter (== x)
+count x  =  length . filter (== x)
 
 main :: IO ()
-main = do
+main  =  do
   chk $ prop_sortOrdered -:> [int]
   chk $ prop_sortCount   -:>  int
   check  `withConditionSize` 9  $
