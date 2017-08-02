213fa676bc58ea8157c22bf6266da5c678c91989
Rudy Matela <rudy@matela.com.br>
2017-08-02 17:39:41 +0100

    test-derive: add first derivation tests
    includes some non-passing commented tests

--- tests/test-derive.hs
+++ tests/test-derive.hs
@@ -1,6 +1,71 @@
+{-# LANGUAGE TemplateHaskell #-}
 -- Copyright (c) 2017 Rudy Matela.
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 import Test
+
+-- List isomorphic to []
+data List a = Cons a (List a)
+            | Nil
+  deriving (Show, Eq, Ord)
+
+-- Perhaps isomorphic to Maybe
+data Perhaps a = Naught
+               | Simply a
+  deriving (Show, Eq, Ord)
+
+-- Ship is isomorphic to Either
+data Ship a b = Port a
+              | Starboard b
+  deriving (Show, Eq, Ord)
+
+-- Arrangement is isomorphic to Ordering
+data Arrangement = Lesser
+                 | Equal
+                 | Greater
+  deriving (Show, Eq, Ord)
+
+data NonEmptyList a = Buil a (NonEmptyList a)
+                    | Unit a
+  deriving (Show, Eq, Ord)
+
+data Mutual a = Mutual a (Shared a) | M a deriving (Show, Eq, Ord)
+data Shared a = Shared (Mutual a) a | S a deriving (Show, Eq, Ord)
+
+data Tree a  = Node (Tree a) a (Tree a) | Empty
+  deriving (Show, Eq, Ord)
+
+data Leafy a = Branch (Leafy a) (Leafy a) | Leaf a
+  deriving (Show, Eq, Ord)
+
+
+-- TODO: auto-derive Listable from deriveGeneralizable?
+deriveListable ''List
+deriveListable ''Perhaps
+deriveListable ''Ship
+deriveListable ''Arrangement
+
+deriveListable ''NonEmptyList
+
+deriveListable ''Mutual
+deriveListable ''Shared
+
+deriveListable ''Tree
+deriveListable ''Leafy
+
+
+-- deriveGeneralizable ''List         -- TODO: make this work
+-- deriveGeneralizable ''Perhaps      -- TODO: make this work
+-- deriveGeneralizable ''Ship         -- TODO: make this work
+-- deriveGeneralizable ''Arrangement  -- TODO: make this work
+
+deriveGeneralizable ''NonEmptyList
+
+deriveGeneralizable ''Mutual
+deriveGeneralizable ''Shared
+
+-- deriveGeneralizable ''Tree         -- TODO: make this work
+deriveGeneralizable ''Leafy
+
 
 main :: IO ()
 main = mainTest tests 10000
@@ -8,4 +73,5 @@ main = mainTest tests 10000
 tests :: Int -> [Bool]
 tests n =
   [ True
+-- TODO: add tests of isomorphicity
   ]
