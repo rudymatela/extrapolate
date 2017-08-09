a477c6719f0cd5fe2eb1640f3c8b7261e09e3bbd
Rudy Matela <rudy@matela.com.br>
2017-08-09 09:57:10 +0100

    All now really exports all

--- mk/All.hs
+++ mk/All.hs
@@ -1,6 +1,20 @@
 module All
   ( module Test.Extrapolate
+  , module Test.Extrapolate.Basic
+  , module Test.Extrapolate.Core
+  , module Test.Extrapolate.Derive
+  , module Test.Extrapolate.Exprs
+  , module Test.Extrapolate.IO
+  , module Test.Extrapolate.TypeBinding
+  , module Test.Extrapolate.Utils
   )
 where
 
 import Test.Extrapolate
+import Test.Extrapolate.Basic
+import Test.Extrapolate.Core
+import Test.Extrapolate.Derive
+import Test.Extrapolate.Exprs
+import Test.Extrapolate.IO
+import Test.Extrapolate.TypeBinding
+import Test.Extrapolate.Utils
