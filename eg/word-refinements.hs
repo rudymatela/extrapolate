e5f1cae10d778f73d2df5f78fbcc4ea446dd56e0
Rudy Matela <rudy@matela.com.br>
2017-11-21 17:31:57 +0000

    eg/word-refs: rename function to match paper

--- eg/word-refinements.hs
+++ eg/word-refinements.hs
@@ -16,12 +16,12 @@ prop_lengthWords1 s  =  s /= "" && not (isSpace (head s))
                     ==> length (words s) == length (filter isSpace s) + 1
 
 prop_lengthWords2 :: String -> Bool
-prop_lengthWords2 s  =  noSurroundingOrDoubleSpace s
+prop_lengthWords2 s  =  noLeadingTrailingOrDoubleSpace s
                     ==> length (words s) == length (filter isSpace s) + 1
   where
-  noSurroundingOrDoubleSpace s = and [ not (isSpace a && isSpace b)
-                                     | let s' = " " ++ s ++ " "
-                                     , (a,b) <- zip s' (tail s') ]
+  noLeadingTrailingOrDoubleSpace s = and [ not (isSpace a && isSpace b)
+                                         | let s' = " " ++ s ++ " "
+                                         , (a,b) <- zip s' (tail s') ]
 
 main :: IO ()
 main = do
