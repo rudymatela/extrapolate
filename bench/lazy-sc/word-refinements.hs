639f82d79e047092465408c9ecf34c16da337ee8
Rudy Matela <rudy@matela.com.br>
2017-11-28 17:14:13 +0000

    LazySmallCheck2012/word-refinements: tweak enum.

--- bench/lazy-sc/word-refinements.hs
+++ bench/lazy-sc/word-refinements.hs
@@ -1,9 +1,34 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 -- Copyright (c) 2017 Colin Runciman and Rudy Matela Braquehais.
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 import Test.LazySmallCheck2012
 import Test.LazySmallCheck2012.TH
+import Data.Data
+import Data.Typeable
+
 import Data.List
 import Data.Char
+
+-- LazySmallCheck2012's Char enumeration does not contain spaces.  So we have
+-- to create types to manually include them.
+
+newtype CharS = CharS { unCharS :: Char }
+  deriving (Eq, Ord, Data, Typeable)
+
+newtype StringS = StringS { unStringS :: String }
+  deriving (Eq, Ord, Data, Typeable)
+
+instance Show CharS where
+  show (CharS c) = show c
+
+instance Show StringS where
+  show (StringS s) = show s
+
+instance Serial CharS where
+  series = drawnFrom $ \d -> map CharS . take (d + 1) $ ' ':['a'..]
+
+instance Serial StringS where
+  series = (StringS . map unCharS) <$> series
 
 prop_lengthWords0 :: String -> Bool
 prop_lengthWords0 s  =  s /= ""
@@ -24,5 +49,7 @@ prop_lengthWords2 s  =  noLeadingTrailingOrDoubleSpace s
 
 main :: IO ()
 main = do
-  test prop_lengthWords0
+  test (prop_lengthWords0 . unStringS)
+--test (prop_lengthWords1 . unStringS)
+--test prop_lengthWords0
 --test prop_lengthWords1
