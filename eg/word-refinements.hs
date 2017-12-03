941f1c6606a22293765002dbf8096e6bca774f63
Rudy Matela <rudy@matela.com.br>
2017-12-03 18:35:54 +0000

    word-refinements: refactor prop_lengthWords2

--- eg/word-refinements.hs
+++ eg/word-refinements.hs
@@ -16,12 +16,11 @@ prop_lengthWords1 s  =  s /= "" && not (isSpace (head s))
                     ==> length (words s) == length (filter isSpace s) + 1
 
 prop_lengthWords2 :: String -> Bool
-prop_lengthWords2 s  =  noLeadingTrailingOrDoubleSpace s
+prop_lengthWords2 s  =  noDoubleSpace (" " ++ s ++ " ")
                     ==> length (words s) == length (filter isSpace s) + 1
   where
-  noLeadingTrailingOrDoubleSpace s = and [ not (isSpace a && isSpace b)
-                                         | let s' = " " ++ s ++ " "
-                                         , (a,b) <- zip s' (tail s') ]
+  noDoubleSpace s = and [ not (isSpace a && isSpace b)
+                        | (a,b) <- zip s (tail s) ]
 
 main :: IO ()
 main = do
